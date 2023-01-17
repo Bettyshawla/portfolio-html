f527c58b66e47b02b2657fcd634c1d8c32ee42ff
BettyShawlagit config --global user.email Betty.tada1@gmail.com <betty.tada1@gmail.com>
2023-01-17 11:16:37 -0500

    portfolio header and body completed

---
+++ script.js
@@ -0,0 +1,23 @@
+let tablinks = document.getElementsByClassName("tab-links")
+let tabContent = document.getElementsByClassName("tab-content")
+
+// onclick
+
+let technicalSkill = document.getElementById("technicalSkill")
+let personalSkill = document.getElementById("personalSkill")
+let experience = document.getElementById("experience")
+let education = document.getElementById("education")
+
+function open(tabname){
+    // for(tablink of tablinks){
+    //     tablink.classList.remove("active-link")
+    // }
+    // for(tabContents of tabContent){
+    //     tabContents.classList.remove("active-link")
+    // }
+    // for(tablink of tablinks){
+    //     tablink.classList.remove("active-link")
+    // }
+
+
+} 
